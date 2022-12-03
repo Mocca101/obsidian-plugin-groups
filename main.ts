c07292866eb28930011a9d5fe99e49dea4fd72a3
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2022-12-03 15:32:38 +0100

    [WIP] Created Basic Settings Components

--- main.ts
+++ main.ts
@@ -1,32 +1,34 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+
+interface PluginInfo {
+	id: string,
+	name: string,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface PluginGroup {
+	name: string;
+	plugins: PluginInfo[];
+	active: boolean;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface PluginGroupsSettings {
+
+	groups: PluginGroup[];
+
+}
+
+const DEFAULT_SETTINGS: PluginGroupsSettings = {
+	groups: []
+}
+
+export default class PluginGroupsMain extends Plugin {
+	settings: PluginGroupsSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -66,13 +68,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new BasicGroupSettingsTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -107,31 +103,181 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class PluginGroupEditModal extends Modal {
+	availablePlugins: PluginInfo[];
 
-	constructor(app: App, plugin: MyPlugin) {
+	groupToEdit: PluginGroup;
+
+	settingsTab: BasicGroupSettingsTab
+
+	constructor(app: App, settingsTab: BasicGroupSettingsTab, availablePlugin: PluginInfo[], group: PluginGroup) {
+		super(app);
+		this.settingsTab = settingsTab;
+		this.availablePlugins = availablePlugin;
+		this.groupToEdit = group;
+	}
+
+	onOpen() {
+		const {contentEl} = this;
+
+		contentEl.empty();
+
+		contentEl.createEl('h2', {text: 'Creating group: ' +  this.groupToEdit.name});
+
+
+		this.availablePlugins.forEach(info => {
+			new Setting(contentEl)
+				.setName(info.name)
+				.addToggle(tgl => {
+					tgl.onChange(doInclude => {
+						this.togglePluginForGroup(info, doInclude);
+					})
+					tgl.setValue(this.groupToEdit.plugins.contains(info))
+				})
+		})
+
+		new Setting(contentEl).addButton(btn => {
+			btn.setButtonText('Cancel');
+			btn.onClick(() => this.close());
+		})
+		new Setting(contentEl).addButton(btn => {
+			btn.setButtonText('Save');
+			btn.onClick(() => this.saveAndClose());
+		})
+	}
+
+	async saveAndClose() {
+		await this.saveChanges();
+		this.close();
+	}
+
+	togglePluginForGroup(plugin: PluginInfo, doInclude: boolean) {
+
+		if(doInclude) {
+			if(this.groupToEdit.plugins.contains(plugin))	return;
+
+			this.groupToEdit.plugins.push(plugin);
+		}
+		else {
+			if(!this.groupToEdit.plugins.contains(plugin)) return;
+
+			this.groupToEdit.plugins.remove(plugin);
+		}
+	}
+
+	onClose() {
+		const {contentEl} = this;
+		contentEl.empty();
+	}
+
+	async saveChanges() {
+		this.settingsTab.plugin.settings.groups.push(this.groupToEdit);
+
+		await this.settingsTab.plugin.saveSettings();
+		this.settingsTab.display();
+	}
+}
+
+class BasicGroupSettingsTab extends PluginSettingTab {
+	plugin: PluginGroupsMain;
+
+	newGroupName: string;
+
+	availablePlugins: PluginInfo[];
+
+
+	constructor(app: App, plugin: PluginGroupsMain) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.loadAvailablePlugins();
+
 	}
 
 	display(): void {
+		this.loadAvailablePlugins();
+
 		const {containerEl} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h5', {text: 'Groups'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Groups')
+			.setDesc('Your plugin Groups')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				.setPlaceholder('Group name')
+				.onChange(val => {
+					this.newGroupName = val;
+				})
+			)
+			.addButton(btn => btn
+				.setIcon('plus')
+				.onClick(() => this.addNewGroup())
+				)
+
+		for (let i = 0; i < this.plugin.settings.groups.length; i++) {
+			const group = this.plugin.settings.groups[i];
+
+			new Setting(containerEl)
+				.setName(group.name)
+				.addToggle(tgl => {
+					tgl.setValue(group.active);
+					tgl.onChange(() => this.toggleGroup(i))
+				})
+				.addButton(btn => {
+					btn.setIcon('minus')
+					btn.onClick(() => this.removeGroup(group))
+				})
+		}
+
 	}
+
+	async toggleGroup(groupID: number) {
+		this.plugin.settings.groups[groupID].active = !this.plugin.settings.groups[groupID].active;
+		await this.plugin.saveSettings();
+		this.display();
+	}
+
+	async addNewGroup() {
+
+		const newGroup: PluginGroup = {
+			name: this.newGroupName,
+			plugins: [],
+			active: false
+		};
+
+		new PluginGroupEditModal(this.app, this, this.availablePlugins, newGroup).open();
+
+
+		this.newGroupName = '';
+	}
+
+	async removeGroup(group: PluginGroup) {
+		this.plugin.settings.groups.remove(group);
+
+		console.log('Removed group', group);
+
+		await this.plugin.saveSettings();
+		this.display();
+	}
+
+	loadAvailablePlugins() {
+		// @ts-ignore
+		const manifests = this.app.plugins.manifests;
+
+		const plugins: PluginInfo[] = [];
+
+
+		for(const manifest in manifests) {
+			const info: PluginInfo = {
+				id: manifests[manifest].id,
+				name: manifests[manifest].name,
+			}
+			plugins.push(info)
+		}
+
+		this.availablePlugins = plugins;
+	}
+
 }
