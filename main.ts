3b4968b8f2961a39905fa701f22e051788726e0b
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2023-01-21 14:33:00 +0100

    Added logging to view load Time of different parts of the plugin

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {Plugin} from 'obsidian';
+import {Notice, Plugin} from 'obsidian';
 import GroupSettingsTab from "./src/GroupSettingsTab";
 import {disableStartupTimeout} from "./src/Utils/Constants";
 import Manager from "./src/Managers/Manager";
@@ -7,26 +7,35 @@ import PluginManager from "./src/Managers/PluginManager";
 
 export default class PgMain extends Plugin {
 
-	readonly doLogTime: boolean = false;
-
 	async onload() {
+		const times: {
+			label: string;
+			time: number
+		}[] = [];
+
+		const time = this.getRoundedCurrentTime() ;
+		times.push({label: 'Time on Load', time: time });
+
 		await Manager.getInstance().init(this);
-		if(this.doLogTime) { console.log('pg-afterManagerInit', window.performance.now() / 1000 ); }
+		this.logTime('Manager Setup', times);
 
 		PluginManager.loadNewPlugins();
-		if(this.doLogTime) { console.log('pg-afterNewPluginLoad', window.performance.now() / 1000 ); }
+		this.logTime('Loading new plugins', times);
 
 		this.addSettingTab(new GroupSettingsTab(this.app, this));
-		if(this.doLogTime) { console.log('pg-afterAddSettings', window.performance.now() / 1000 ); }
-
+		this.logTime('Creating the Settings Tab', times);
 
 		if(!Manager.getInstance().groupsMap) {
+			this.displayTimeNotice(times);
+
 			return; // Exit early if there are no groups yet, no need to load the rest.
 		}
 
 		if(Manager.getInstance().generateCommands) {
 			Manager.getInstance().groupsMap.forEach(group => CommandManager.getInstance().AddGroupCommands(group.id));
-			if(this.doLogTime) { console.log('pg-afterGenerateCommand', window.performance.now() / 1000 ); }
+			if(Manager.getInstance().logDetailedTime) {
+				this.logTime('Generated Commands for Groups in', times);
+			}
 		}
 
 		// TODO: Improve hacky solution if possible
@@ -34,11 +43,42 @@ export default class PgMain extends Plugin {
 			Manager.getInstance().groupsMap.forEach(group => {
 				if (group.loadAtStartup) group.startup();
 			});
-		}
-		if(this.doLogTime) { console.log('pg-afterInitStartups', window.performance.now() / 1000 ); }
 
+			if(Manager.getInstance().logDetailedTime) {
+				this.logTime('Dispatching Groups for delayed start in', times);
+			}
+		}
+
+		this.displayTimeNotice(times);
 	}
 
+	private logTime(label: string, times: {label: string, time: number}[]) {
+		if(Manager.getInstance().logDetailedTime) {
+			times.push({label, time: this.elapsedTime(times)});
+		}
+	}
+
+	private displayTimeNotice(times: {label: string, time: number}[] ) {
+		if(!Manager.getInstance().logDetailedTime || times.length === 0) { return }
+
+		const totalTime = Math.round(this.accTime(times.slice(1)) / 1000);
+
+		new Notice(times.map(item => item.label + ': ' + item.time + ' μs').join('\n') + '\nTotal Time: ' + totalTime + ' ms', 5000);
+	}
+
+	private elapsedTime(times: {label: string, time: number}[] ) : number {
+		if(times.length > 1) {
+			return this.getRoundedCurrentTime() - this.accTime(times);
+		}
+		return this.getRoundedCurrentTime() - times[0].time;
+	}
+
+	private accTime(times: {label: string, time: number}[]): number {
+		return times.map(item => item.time).reduce((prev, curr) => prev + curr);
+	}
+	private getRoundedCurrentTime() : number {
+		return Math.round(window.performance.now() * 1000)
+	}
 
 	onunload() {
 	}
