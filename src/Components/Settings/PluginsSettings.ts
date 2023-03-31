c595beb6ad32a7b23ac286239d1a2325e4bf06c9
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2023-03-31 11:06:36 +0200

    PluginSettings UX Imroved: Filter & Sort Funtctionality

--- src/Components/Settings/PluginsSettings.ts
+++ src/Components/Settings/PluginsSettings.ts
@@ -1,11 +1,14 @@
 import { makeCollapsible } from "src/Utils/Utilities";
 import HtmlComponent from "../BaseComponents/HtmlComponent";
-import { ExtraButtonComponent } from "obsidian";
+import { ExtraButtonComponent, Setting } from "obsidian";
 import EditPluginList from "../EditPluginList";
 import { PgPlugin } from "src/DataStructures/PgPlugin";
 import Manager from "src/Managers/Manager";
 import PluginManager from "src/Managers/PluginManager";
 import { ItemAndDescription } from "../DescriptionsList";
+import { PluginGroup } from "src/DataStructures/PluginGroup";
+import DropdownActionButton, { DropdownOption } from "../BaseComponents/DropdownActionButton";
+import FilteredGroupsList from "../FilteredGroupsList";
 
 export interface PluginSettingOptions {
 	collapsible?: boolean;
@@ -16,6 +19,18 @@ export interface PluginSettingOptions {
 export default class PluginSettings extends HtmlComponent<PluginSettingOptions> {
 
   content: HTMLElement;
+
+  private filteredPlugins: ItemAndDescription<PgPlugin>[];
+
+	searchTerm: string;
+
+  private pluginsList: EditPluginList;
+
+	private filteredGroups: Map<string, PluginGroup> = new Map<
+		string,
+		PluginGroup
+	>();
+
 
 	constructor(parentEL: HTMLElement, options: PluginSettingOptions) {
 		super(parentEL, options);
@@ -46,6 +61,10 @@ export default class PluginSettings extends HtmlComponent<PluginSettingOptions> 
 		listContainer.style.maxHeight =
 			(this.options.maxListHeight?.toString() ?? '') + 'px';
 
+		this.FilterAndSort();
+
+		this.createFilterSection(listContainer);
+
 		this.GeneratePluginsList(listContainer);
 	}
 
@@ -57,21 +76,21 @@ export default class PluginSettings extends HtmlComponent<PluginSettingOptions> 
 			'Refresh list for changes to the plugins and assigned groups.'
 		);
 
-		const pluginList = new EditPluginList(
+		this.pluginsList = new EditPluginList(
 			this.content,
 			{
-				items: this.getPluginsWithGroupsAsDescription(),
+				items: this.filteredPlugins,
 			},
 			() => {
-				pluginList.update({
-					items: this.getPluginsWithGroupsAsDescription(),
+				this.pluginsList.update({
+					items: this.filteredPlugins,
 				});
 			}
 		);
 
 		refresh.onClick(() => {
-			pluginList.update({
-				items: this.getPluginsWithGroupsAsDescription(),
+			this.pluginsList.update({
+				items: this.filteredPlugins,
 			});
 		});
 	}
@@ -85,5 +104,145 @@ export default class PluginSettings extends HtmlComponent<PluginSettingOptions> 
 				description: groups.map((group) => group.name).join(', '),
 			};
 		});
+	}
+
+  // ------------------------------------------------------------------------
+
+  private createFilterSection(parentEl: HTMLElement): HTMLElement {
+		const filterSection = parentEl.createDiv();
+		new Setting(filterSection).setName('Search').addText((txt) => {
+			txt.setPlaceholder('Search for Plugin...');
+			txt.onChange((search) => {
+				this.searchPlugins(search);
+			});
+		});
+
+		const filtersAndSelectionContainer = filterSection.createDiv({
+			cls: 'pg-plugin-filter-container',
+		});
+		const filtersAndSelection = filtersAndSelectionContainer.createDiv({
+			cls: 'pg-plugin-filter-section',
+		});
+		const filters = filtersAndSelection.createDiv();
+
+		const filteredGroupsChips = new FilteredGroupsList(
+			filtersAndSelectionContainer,
+			this.filteredGroups,
+			() => this.OnFilterOrSortUpdated()
+		);
+
+		const toggleGroupFilter = (group: PluginGroup) => {
+			this.filteredGroups.has(group.id)
+				? this.filteredGroups.delete(group.id)
+				: this.filteredGroups.set(group.id, group);
+		};
+		const updateGroupFilters = () => {
+			filteredGroupsChips.update(this.filteredGroups);
+			this.OnFilterOrSortUpdated();
+		};
+
+		const groupFilterOptions: DropdownOption[] = [
+			{
+				label: 'All groups',
+				func: () => {
+					if (
+						this.filteredGroups.size ===
+						Manager.getInstance().groupsMap.size
+					) {
+						this.filteredGroups.clear();
+					} else {
+						this.filteredGroups = new Map(
+							Manager.getInstance().groupsMap
+						);
+					}
+					updateGroupFilters();
+				},
+			},
+		];
+		Manager.getInstance().groupsMap.forEach((group) => {
+			groupFilterOptions.push({
+				label: group.name,
+				func: () => {
+					toggleGroupFilter(group);
+					updateGroupFilters();
+				},
+			});
+		});
+
+		new DropdownActionButton(filters, {
+			mainLabel: {
+				label: 'Filter Groups',
+			},
+			dropDownOptions: groupFilterOptions,
+			drpIcon: 'filter',
+		});
+
+		return filterSection;
+	}
+
+	// Cumulative Filter function called from various points that acts depending on filter variables set at object level
+	private OnFilterOrSortUpdated() {
+		this.FilterAndSort();
+
+		this.showFilteredPlugins();
+	}
+
+	private FilterAndSort() {
+		this.filteredPlugins = this.getPluginsWithGroupsAsDescription();
+		if (this.searchTerm && this.searchTerm !== '') {
+			this.filteredPlugins = this.filteredPlugins.filter((p) => p.item.name.toLowerCase().contains(this.searchTerm.toLowerCase())
+			);
+		}
+
+		if (this.filteredGroups.size > 0) {
+			this.filteredPlugins = this.filterPluginsByGroups(
+				this.filteredPlugins,
+				this.filteredGroups
+			);
+		}
+
+		this.filteredPlugins = this.sortPlugins(
+			this.filteredPlugins
+		);
+	}
+
+	private filterPluginsByGroups(
+		pluginsToFilter: ItemAndDescription<PgPlugin>[],
+		groupsToExclude: Map<string, PluginGroup>
+	): ItemAndDescription<PgPlugin>[] {
+		const pluginMembershipMap =
+			Manager.getInstance().mapOfPluginsDirectlyConnectedGroups;
+
+		return pluginsToFilter.filter((itemAndDesc) => {
+			if (!pluginMembershipMap.has(itemAndDesc.item.id)) {
+				return true;
+			}
+
+			for (const groupId of pluginMembershipMap.get(itemAndDesc.item.id) ?? []) {
+				if (groupsToExclude.has(groupId)) {
+					return false;
+				}
+			}
+			return true;
+		});
+	}
+
+	private searchPlugins(search: string) {
+		this.searchTerm = search;
+		this.OnFilterOrSortUpdated();
+		this.showFilteredPlugins();
+	}
+
+	private showFilteredPlugins() {
+		this.pluginsList.update({ items: this.filteredPlugins});
+	}
+
+  sortPlugins(plugins: ItemAndDescription<PgPlugin>[]): ItemAndDescription<PgPlugin>[] {
+		if (!plugins || !(typeof plugins[Symbol.iterator] === 'function')) {
+			return [];
+		}
+		const sortedArray = [...plugins];
+
+		return sortedArray.sort((a, b) => a.item.name.localeCompare(b.item.name));
 	}
 }
