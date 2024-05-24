0a09f106e9e8bc3712ac90ad9673065e3cfe5e0e
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-05-24 12:43:20 +0200

    Reusable wrapper for Setting Groups

--- src/Components/Settings/main-settings.svelte
+++ src/Components/Settings/main-settings.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
-	import { Collapsible } from "bits-ui";
 	import GeneralSettings from "./general-settings.svelte";
-	import { ChevronDown, ChevronUp } from "lucide-svelte";
-  let collapsibleOpen = true;
-
+	import SettingsGroup from "./settings-group.svelte";
 
 </script>
 <h1>Plugin Groups</h1>
-<Collapsible.Root bind:open={collapsibleOpen}>
-	<Collapsible.Trigger class="flex justify-between w-full mb-2 py-2">
-		<h4>General Settings</h4>
-		{#if collapsibleOpen}
-			<ChevronDown size="24" />
-		{:else}
-				<ChevronUp size="24" />
-		{/if}
-	</Collapsible.Trigger>
-	<Collapsible.Content class="p-2">
-		<GeneralSettings />
-	</Collapsible.Content>
-</Collapsible.Root>
+<SettingsGroup title="General Settings">
+	<GeneralSettings />
+</SettingsGroup>
---
+++ src/Components/Settings/settings-group.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Collapsible } from "bits-ui";
+	import { ChevronDown, ChevronUp } from "lucide-svelte";
+  let collapsibleOpen = true;
+
+	export let title = "Settings";
+
+</script>
+<Collapsible.Root bind:open={collapsibleOpen}>
+	<Collapsible.Trigger class="flex justify-between w-full py-2 mb-2">
+		<h4>{title}</h4>
+		{#if collapsibleOpen}
+			<ChevronDown size="24" />
+		{:else}
+				<ChevronUp size="24" />
+		{/if}
+	</Collapsible.Trigger>
+	<Collapsible.Content class="p-2">
+		<slot />
+	</Collapsible.Content>
+</Collapsible.Root>
