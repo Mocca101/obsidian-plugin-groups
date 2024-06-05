499ef924aa1bf6601fa6dfffecdc2817f5f62b4c
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-06-05 23:41:36 +0200

    Create svelte component for text input

---
+++ src/Components/BaseComponents/obs-text.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let value: string;
+	export let placeholder: string = "";
+
+</script>
+<input type="text" spellcheck="false" placeholder={placeholder} bind:value={value} />
