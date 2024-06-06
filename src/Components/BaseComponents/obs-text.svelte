3e75100b4d8046bc359ff7211ea571f7dec673e4
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-06-06 23:59:22 +0200

    add submit event option to Text Field

--- src/Components/BaseComponents/obs-text.svelte
+++ src/Components/BaseComponents/obs-text.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	export let value: string;
 	export let placeholder: string = "";
+	export let onSubmit: () => void = () => {};
+
+	const onKeyDown = (e: KeyboardEvent) => {
+		if (e.key === "Enter") {
+			onSubmit();
+		}
+	};
+
 
 </script>
-<input type="text" spellcheck="false" placeholder={placeholder} bind:value={value} />
+<input type="text" spellcheck="false" placeholder={placeholder} bind:value={value} on:keydown={onKeyDown} />
