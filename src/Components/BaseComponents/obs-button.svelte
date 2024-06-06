d9357835aa37f24ccb8bd28bcc65656193ce3c07
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-06-07 00:17:26 +0200

    Add Tooltip option for button

--- src/Components/BaseComponents/obs-button.svelte
+++ src/Components/BaseComponents/obs-button.svelte
@@ -3,9 +3,10 @@
 	export let icon: any | null = null;
 	export let onClick: () => void;
 	export let disabled: boolean = false;
+	export let tooltip: string | null = null;
 
 </script>
-<button on:click={onClick} disabled={disabled} >
+<button on:click={onClick} disabled={disabled} aria-label={tooltip} >
 	{#if title}
 		{title}
 	{:else if icon}
